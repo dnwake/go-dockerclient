8b9dc1a032475178a37b98082a27553b9bf5e3b0
David Wake <dwake@box.com>
2016-04-11 19:17:33 -0700

    Add stuff for Notary delegation

--- content_trust_integration_testing/image_pusher.mk
+++ content_trust_integration_testing/image_pusher.mk
@@ -1,6 +1,8 @@
 ## This container is responsible for pushing images to the registry
 export IMAGE_PUSHER_CONTAINER_NAME=image_pusher
+export IMAGE_PUSHER_2_CONTAINER_NAME=image_pusher_2
 export IMAGE_PUSHER_LOG=$(TMPDIR)/image_pusher_log
+export IMAGE_PUSHER_2_LOG=$(TMPDIR)/image_pusher_2_log
 
 push_images: image_pusher good_image bad_image registry notary
 	echo "If necessary, pushing good, bad and corrupted images to the registry.  May take some time..."
@@ -16,7 +18,8 @@ push_images: image_pusher good_image bad_image registry notary
 			docker push $(REGISTRY_URL)/$(GOOD_IMAGE_NAME):latest >/dev/null && \
 			docker push $(REGISTRY_URL)/$(BAD_IMAGE_NAME):latest >/dev/null && \
 			docker push $(REGISTRY_URL)/$(CORRUPT_GOOD_IMAGE_NAME):latest >/dev/null; \
-		fi \
+		fi; \
+                
 	'
 
 	echo "Corrupting $(CORRUPT_GOOD_IMAGE_NAME) so it actually points to $(BAD_IMAGE_NAME) in the registry"
@@ -27,9 +30,34 @@ push_images: image_pusher good_image bad_image registry notary
 		 /var/lib/registry/docker/registry/v2/repositories/$(CORRUPT_GOOD_IMAGE_NAME) \
 	'
 
+push_images_2: image_pusher_2 good_image bad_image registry notary
+	echo "If necessary, pushing good, bad and corrupted images to the registry.  May take some time..."
+	docker exec $(IMAGE_PUSHER_2_CONTAINER_NAME) bash -c ' \
+			export DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE=root; \
+			export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=repository; \
+			export DOCKER_CONTENT_TRUST=1; \
+			export DOCKER_CONTENT_TRUST_SERVER=$(NOTARY_URL); \
+			docker tag -f $(GOOD_IMAGE_NAME) $(REGISTRY_URL)/$(GOOD_IMAGE_NAME):latest && \
+			docker tag -f $(GOOD_IMAGE_NAME) $(REGISTRY_URL)/$(CORRUPT_GOOD_IMAGE_NAME):latest && \
+			docker tag -f $(BAD_IMAGE_NAME) $(REGISTRY_URL)/$(BAD_IMAGE_NAME):latest && \
+			docker push $(REGISTRY_URL)/$(GOOD_IMAGE_NAME):latest >/dev/null && \
+			docker push $(REGISTRY_URL)/$(BAD_IMAGE_NAME):latest >/dev/null && \
+			docker push $(REGISTRY_URL)/$(CORRUPT_GOOD_IMAGE_NAME):latest >/dev/null; \
+	'
 image_pusher: notary registry client_image etc_hosts
 	$(DOCKER_RUN) \
 		$(IMAGE_PUSHER_CONTAINER_NAME) \
+		$(IMAGE_PUSHER_LOG) \
+		-t \
+		--privileged \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		--link $(NOTARY_CONTAINER_NAME_1):$(NOTARY_HOST) \
+		--link $(REGISTRY_CONTAINER_NAME):$(REGISTRY_HOST) \
+		$(CLIENT_IMAGE_NAME)
+
+image_pusher_2: notary registry client_image etc_hosts
+	$(DOCKER_RUN) \
+		$(IMAGE_PUSHER_2_CONTAINER_NAME) \
 		$(IMAGE_PUSHER_LOG) \
 		-t \
 		--privileged \
